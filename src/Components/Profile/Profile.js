b0d92d6f7235523a80d850d552dbd3ae41e323b5
roh8raut <roh8raut@gmail.com>
2019-05-11 17:32:31 +0530

    Resolved players profile issue for tablet view

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -3,23 +3,23 @@ import './Profile.css'
 
 // let count = 0;
 const Profile = (props) => {
-   
-        return (
-                    <div className="col-md-4 col-xs-6 player-info">
-                        <img src={props.player.imageURL} alt="profile-pic" className="profile-pic"/>
-                        <div className="info">
-                            <b>Name:</b ><span className="details">{props.player.fullName}</span><br />
-                            <b>Nationality:</b><span className="details">{props.player.nationality}</span><br />
-                            
-                            <b>Position:</b><span >{props.player.position}</span><br />
 
-                            <b>Jersey Number:</b><span>{props.player.shirtNumber}</span><br/>
+    return (
+        <div className="col-md-4 col-xs-6 col-sm-3 player-info">
+            <img src={props.player.imageURL} alt="profile-pic" className="profile-pic" />
+            <div className="info">
+                <b>Name:</b ><span className="details">{props.player.fullName}</span><br />
+                <b>Nationality:</b><span className="details">{props.player.nationality}</span><br />
 
-                            <b>Age:</b><span >{props.player.age}</span>
-                        </div>
-                    </div>
-               
-        )
+                <b>Position:</b><span >{props.player.position}</span><br />
+
+                <b>Jersey Number:</b><span>{props.player.shirtNumber}</span><br />
+
+                <b>Age:</b><span >{props.player.age}</span>
+            </div>
+        </div>
+
+    )
 
 }
 
